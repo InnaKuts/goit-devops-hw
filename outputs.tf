16f8d6319d419c7272d6856a4e3419d67e53a8fd
Inna Kuts <inna.v.kuts@gmail.com>
2026-04-25 13:26:10 +0200

    jenkins on micro

--- outputs.tf
+++ outputs.tf
@@ -58,15 +58,15 @@ output "eks_ebs_csi_addon_version" {
   value       = module.eks.ebs_csi_addon_version
 }
 
-# output "jenkins_release" {
-#   description = "Helm release name for Jenkins"
-#   value       = module.jenkins.jenkins_release_name
-# }
+output "jenkins_release" {
+  description = "Helm release name for Jenkins"
+  value       = module.jenkins.jenkins_release_name
+}
 
-# output "jenkins_namespace" {
-#   description = "Kubernetes namespace for Jenkins"
-#   value       = module.jenkins.jenkins_namespace
-# }
+output "jenkins_namespace" {
+  description = "Kubernetes namespace for Jenkins"
+  value       = module.jenkins.jenkins_namespace
+}
 
 output "argo_cd_namespace" {
   description = "Kubernetes namespace for Argo CD"
