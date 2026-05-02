3d43d4454bdc364032ea88a5a2d06b4d8dad56ce
Inna Kuts <inna.v.kuts@gmail.com>
2026-05-02 13:43:28 +0200

    s3 backend

--- backend.tf
+++ backend.tf
@@ -20,13 +20,13 @@ terraform {
     }
   }
 
-  backend "local" {}
+  # backend "local" {}
 
-  # backend "s3" {
-  #   bucket       = "goit-devops-hw-tfstate-001001"
-  #   key          = "goit-devops-hw/terraform.tfstate"
-  #   region       = "eu-north-1"
-  #   encrypt      = true
-  #   use_lockfile = true
-  # }
+  backend "s3" {
+    bucket       = "goit-devops-hw-tfstate-001001"
+    key          = "goit-devops-hw/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
